55092a251f646c48e5f3c452acd94576567d6759
leejohy-0223 <leejohy@naver.com>
2022-03-24 00:15:37 +0900

    feat: initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 스프링 게시판 구현 프로젝트
+
+## 사용 기술
+
+- Java 11
+- SpringBoot
+- JPA
+- H2 database
+- mustache
+- spring security
+
+## 구현 기능
---
+++ src/main/java/com/toyPJT/noticeBoard/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.toyPJT.noticeBoard.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ArticleController {
+
+
+}
