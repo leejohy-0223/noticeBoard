bd286e36a30e77dc0c61a8c0aecbc5ec8b064c5e
leejohy-0223 <leejohy@naver.com>
2022-03-25 00:25:36 +0900

    feat: Entity 생성

---
+++ src/main/java/com/toyPJT/noticeBoard/domain/board/Board.java
@@ -0,0 +1,47 @@
+package com.toyPJT.noticeBoard.domain.board;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.toyPJT.noticeBoard.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User user;
+
+
+}
---
+++ src/main/java/com/toyPJT/noticeBoard/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.toyPJT.noticeBoard.domain.user;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
