8851409d7cb6184f5bc3668783a2e904a1e58dd6
leejohy-0223 <leejohy@naver.com>
2022-03-27 14:24:00 +0900

    test: /index 페이지 테스트 변경
    - model check 부분 추가

--- src/test/java/com/toyPJT/noticeBoard/controller/BoardControllerTest.java
+++ src/test/java/com/toyPJT/noticeBoard/controller/BoardControllerTest.java
@@ -1,13 +1,21 @@
 package com.toyPJT.noticeBoard.controller;
 
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.toyPJT.noticeBoard.service.BoardService;
 
 @WebMvcTest(controllers = BoardController.class)
 class BoardControllerTest {
@@ -15,11 +23,19 @@ class BoardControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private BoardService boardService;
+
     @DisplayName("기본 /요청에 대해서는 index view를 반환한다.")
     @Test
     void get_index() throws Exception {
+        // given
+        given(boardService.getBoards())
+            .willReturn(new ArrayList<>());
+
         mockMvc.perform(get("/"))
             .andExpect(status().isOk())
+            .andExpect(model().attributeExists("boards"))
             .andExpect(view().name("index"));
     }
 
